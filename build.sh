a15294250d68ba725c8bc50fa0847afc75a6aacc
Kalebe-Nascimento <kalebeinfo@gmail.com>
2025-05-14 21:32:06 -0300

    add ignore files

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-ECR_REGISTRY="211125625950.dkr.ecr.us-east-1.amazonaws.com/aws_manga"
+ECR_REGISTRY="211125625950.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
 docker build -t $ECR_REGISTRY/aws_manga:latest .
 docker push $ECR_REGISTRY/aws_manga:latest
